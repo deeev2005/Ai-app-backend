08749c47995d0891cd2bb7a0350f7631cf740a47
deeev2005 <deepesh.verma.min24@itbhu.ac.in>
2025-11-30 00:09:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -377,14 +377,7 @@ def _predict_video_wan(image_path: str, prompt: str):
             input_image=handle_file(image_path),
             prompt=prompt,
             steps=8,
-            negative_prompt = """ multiple bodies, overlapping bodies, ghosting, duplicate limbs, extra limbs,
-jitter, unstable motion, warped motion, motion artifacts,
-identity drift, morphing face, morphing identity,
-extra head, extra arms, extra legs,
-multiple poses, pose switching, fast dancing, chaotic dancing,
-motion blur, distorted silhouette, unnatural movement
-"""
-
+            negative_prompt="색조 선명, 과다 노출, 정적, 세부 흐림, 자막, 스타일, 작품, 그림, 화면, 정지, 회색조, 최악 품질, 저품질, JPEG 압축, 추함, 불완전, 추가 손가락, 잘못 그려진 손, 잘못 그려진 얼굴, 기형, 변형, 형태 불량 사지, 손가락 융합, 정지 화면, 지저분한 배경, 세 개의 다리, 배경 사람 많음, 뒤로 걷기, multiple bodies,overlapping figures,ghost limbs,duplicate limbs,deformed motion,jitter,unstable motion,morphing face,morphing identity,extra hands,extra head,multiple poses,duplicated person",
             duration_seconds=3.5,
             guidance_scale=3,
             guidance_scale_2=3,
@@ -726,8 +719,3 @@ if __name__ == "__main__":
         timeout_keep_alive=300,  # 5 minutes keep alive
         timeout_graceful_shutdown=30
     )
-
-
-
-
-
